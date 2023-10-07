9bf8bc2125fc7a6ace18d30bed844729d8f7854d
satriomuslim <satriomuslim2627@gmail.com>
2023-10-07 14:03:48 +0700

    Add api config, api service, and response

---
+++ app/src/main/java/com/example/testmovieapp/api/ApiConfig.kt
@@ -0,0 +1,27 @@
+package com.example.testmovieapp.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+
+    companion object {
+        private var BaseUrl = "https://api.themoviedb.org/3/"
+
+        fun getApiService(): ApiService {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/testmovieapp/api/ApiService.kt
@@ -0,0 +1,15 @@
+package com.example.testmovieapp.api
+
+import com.example.testmovieapp.remote.MovieResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("discover/movie")
+    fun movieApp(
+        @Query("api_key") api_key: String,
+    ) : Call<MovieResponse>
+
+}
