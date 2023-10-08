ea6721feabe55b1fbc28ad50f4f7ae6e86f4a115
satriomuslim <satriomuslim2627@gmail.com>
2023-10-08 14:43:54 +0700

    update main activity

--- app/src/main/java/com/example/testmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/testmovieapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.testmovieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.testmovieapp.databinding.ActivityMainBinding
+import com.example.testmovieapp.ui.MovieAdapter
+import com.example.testmovieapp.viewmodel.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val movieViewModel = MovieViewModel()
+    private lateinit var movieAdapter: MovieAdapter
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        movieAdapter = MovieAdapter(emptyList())
+
+        binding.movieRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = movieAdapter
+        }
+
+        movieViewModel.movieList.observe(this) { movies ->
+            movieAdapter = MovieAdapter(movies)
+            binding.movieRecyclerView.adapter = movieAdapter
+        }
     }
+
 }
